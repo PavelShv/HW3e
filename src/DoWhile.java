3e2398d25c74376865662f87d2d5442d7f4f71ac
pavel.shvedov <composer.shvedov@gmail.com>
2022-01-28 15:47:01 +0300

    Add more examples

---
+++ src/DoWhile.java
@@ -0,0 +1,9 @@
+public class DoWhile {
+    public static void main(String[] args) {
+        int i = 10;
+        do{
+            System.out.println("тик"+i);
+            i--;
+        } while (i>0);
+    }
+}
---
+++ src/For.java
@@ -0,0 +1,12 @@
+public class For {
+    public static void main(String[] args) {
+        int i;
+        int b = 0;
+        for (i=1; i<10; i++) {
+            System.out.print(i+ " проход цикла ");
+            b+=i;
+            System.out.println("b="+b);
+        }
+
+    }
+}
